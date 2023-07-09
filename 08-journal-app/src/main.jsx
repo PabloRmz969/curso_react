ca9f8a4d01a2b20a6d7f3ff7dc987b4390fbed0a
PabloRmz969 <rmzpablo80@gmail.com>
2023-07-08 18:00:32 -0600

    Login and register pages

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp.jsx'
-import './assets/styles.css'
+import './styles.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
